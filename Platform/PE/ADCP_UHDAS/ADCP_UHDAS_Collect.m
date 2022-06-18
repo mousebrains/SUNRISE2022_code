79abc2a4e568fdfccc13656dc7937e0dacab360a
Ale Sanchez-Rios <alesanchezrios.po@gmail.com>
2022-06-17 20:25:08 -0500

    quick ADCP fix
    Fucent and Ale did a quick fix to be able to create new ADCP data for wh300 and wh1200. no data in wh600

--- Platform/PE/ADCP_UHDAS/ADCP_UHDAS_Collect.m
+++ Platform/PE/ADCP_UHDAS/ADCP_UHDAS_Collect.m
@@ -5,14 +5,19 @@ clear
 close all
 
 %%%
+% ADCP_Project_name = 'PE22_31_SHERAMAN';
+% ADCP_name = {'wh300','wh600','wh1200'};
+
 ADCP_Project_name = 'PE22_31_SHERAMAN';
-ADCP_name = {'wh300','wh600','wh1200'};
+ADCP_name = {'wh300','wh1200'};
+
 
 %%% setting path and pre-loading files
 addpath('../_Config')
 Process_Mode = 'ADCP_UHDAS';
 data_path %% all data path and library
 
+ADCP_PROC_Path = ['/Volumes/homes/Science/PE22_31_Shearman/data/Platform/PE/ADCP_UHDAS/'];
 %%
 for i = 1:length(ADCP_name)
     
@@ -21,10 +26,10 @@ for i = 1:length(ADCP_name)
     time_temp = num2str(ADCP_temp.time');
     ADCP_temp.dn = datenum(time_temp,'yyyy  mm  dd  HH  MM  SS')';
     
-    true_heading = repmat(interp1(ship.dn,ship.true_heading,ADCP_temp.dn),[size(ADCP_temp.depth,1) 1 ]);
+%     true_heading = repmat(interp1(ship.dn,ship.true_heading,ADCP_temp.dn),[size(ADCP_temp.depth,1) 1 ]);
     
-    ADCP_temp.uA =  ADCP_temp.u.*cosd(true_heading) + ADCP_temp.v.*sind(true_heading);
-    ADCP_temp.uC =  ADCP_temp.u.*sind(true_heading) - ADCP_temp.v.*cosd(true_heading);
+%     ADCP_temp.uA =  ADCP_temp.u.*cosd(true_heading) + ADCP_temp.v.*sind(true_heading);
+%     ADCP_temp.uC =  ADCP_temp.u.*sind(true_heading) - ADCP_temp.v.*cosd(true_heading);
     
     save([ADCP_PROC_final_Path Prefix '_' ADCP_name{i} '_Processed.mat'],'-struct','ADCP_temp','-v7.3')
 end
